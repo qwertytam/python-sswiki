d667f62fd22b130513e63dffeeb2f6ea251d3477
qwertytam <tom.q.marshall@gmail.com>
2022-08-23 10:48:49 -0400

    Additional functions

--- sswiki/utils.py
+++ sswiki/utils.py
@@ -18,16 +18,20 @@ def incrementDFValues(df):
     return df.mask(df.duplicated(), df.add("_" + counter))
 
 
-def loadVesselData(data_csv):
+def loadVesselData(data_csv, **kwargs):
     """Load vessel data file.
 
     Keyword arguments:
     data_csv -- File path to file
+    **kwargs -- Arguments passed to `read_csv`
 
     Return:
     A pandas data frame with vessel data from the data file.
     """
-    return pd.read_csv(const.DATA_DIR + data_csv, dtype='str')
+    return pd.read_csv(const.DATA_DIR + data_csv,
+                       dtype='str',
+                       encoding="utf-8",
+                       **kwargs)
 
 
 def lengthenMonth(sf):
@@ -52,3 +56,35 @@ def lengthenMonth(sf):
     sf = sf.str.replace(r'^Dec$', 'December', regex=True)
 
     return sf
+
+
+def dfStrNormalize(df):
+    """Normalize unicode normal form for all columns in the data frame
+
+    Keyword arguments:
+    df -- A pandas data frame to normalize
+
+    Return:
+    The string normalized pandas data frame
+    """
+    return df.apply(lambda x: x.str.normalize('NFKC'))
+
+
+def findDFCols(df, cols):
+    """Finds all column names in the data frame that match the format
+    <name>_d where <name> is provided and d is a digit
+
+    Keyword arguments:
+    df -- A pandas data frame with columns to find
+    cols -- List of column names to find
+
+    Return:
+    A pandas Index object with column labels that match
+    """
+    # regex pattern for the duplicate column name suffix - see
+    # incrementDFValues()
+    incr_suffix = r'(?:\_\d+)?'
+    pat = (incr_suffix + "|").join(cols)
+    pat = r'(' + pat + r'(?:\_\d+)?' + r')'
+
+    return df.filter(regex=pat, axis=1).columns
