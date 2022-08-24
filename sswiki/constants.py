bc1ec30b8a39bd01ddf5a500e3b89ec5b37dee39
qwertytam <tom.q.marshall@gmail.com>
2022-08-23 20:03:17 -0400

    Rearrange and clarify comments

--- sswiki/constants.py
+++ sswiki/constants.py
@@ -18,25 +18,7 @@ VL_COLS = ["group_type",
            "group_type_url",
            "vessel_url"]
 
-# Data items from the vessel article infobox that are dates
-DT_SH_COLS = ["Acquired",
-              "Active",
-              "Cancelled",
-              "Christened",
-              "Commissioned",
-              "Completed",
-              "Decommissioned",
-              "Laid down",
-              "Launched",
-              "Lost",
-              "Maiden voyage",
-              "Ordered",
-              "Out of service",
-              "Recommissioned",
-              "Renamed",
-              "Retired",
-              "Stricken"]
-
+# Vessel general characteristic data items
 # Linear measurements e.g. feet, metres
 LNMES_GC_COLS = ["Beam",
                  "Draft",
@@ -50,20 +32,39 @@ WTMES_GC_COLS = ["Displacement",
 # Speed measurements e.g. knots
 SPMES_GC_COLS = ["Speed"]
 
+# All other non-measurement columns
 NONMES_GC_COLS = ["Class and type",
                   "Identification",
                   "Type"]
 
+
+# Vessel service history data items
+# Data items from the vessel article infobox that are dates
+DT_SH_COLS = ["Acquired",
+              "Christened",
+              "Commissioned",
+              "Completed",
+              "Decommissioned",
+              "In service",
+              "Laid down",
+              "Launched",
+              "Maiden voyage",
+              "Ordered",
+              "Out of service",
+              "Recommissioned",
+              "Renamed",
+              "Stricken"]
+
+# All other non-measurement columns
 NONMES_SH_COLS = ["Builder",
                   "Builders",
                   "Built",
                   "Fate",
                   "In commission",
-                  "In service",
                   "Name",
-                  "Preserved",
                   "Reclassified",
                   "Status"]
 
+# Combined column names
 GC_COLS = LNMES_GC_COLS + WTMES_GC_COLS + SPMES_GC_COLS + NONMES_GC_COLS
 SH_COLS = DT_SH_COLS + NONMES_SH_COLS
